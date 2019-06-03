40a961aba2ada84fdf235e7411c0fcab0284347f
Jorge Silva <jorgesilva.me@gmail.com>
2019-06-03 15:55:56 +0100

    give total supply to contract creator

--- contracts/ERC20Test.sol
+++ contracts/ERC20Test.sol
@@ -42,6 +42,10 @@ contract Token {
 
 contract StandardToken is Token {
 
+    constructor() public {
+      balances[msg.sender] = totalSupply;               // Give the creator all initial tokens
+    }
+
     function transfer(address _to, uint256 _value) public returns (bool success) {
       if (balances[msg.sender] >= _value && _value > 0) {
         balances[msg.sender] -= _value;
