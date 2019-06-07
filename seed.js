0440f52617708de4b7e8b7eabcf8b4ff4942cdaa
Jorge Silva <jorgesilva.me@gmail.com>
2019-06-07 19:02:22 +0100

    added cli params

--- seed.js
+++ seed.js
@@ -1,8 +1,9 @@
+const commandLineArgs = require('command-line-args')
 const Web3 = require('web3')
 const Tx = require('ethereumjs-tx')
+const _get = require('lodash/get')
 
 const registryContractInfo = require('./build/contracts/RegistryLookup.json')
-
 const batContractInfo = require('./build/contracts/BAT.json')
 const daiContractInfo = require('./build/contracts/Dai.json')
 const golemContractInfo = require('./build/contracts/Golem.json')
@@ -13,26 +14,34 @@ const wEthContractInfo = require('./build/contracts/WEth.json')
 const zeroXContractInfo = require('./build/contracts/ZeroX.json')
 
 const NETWORK_ID = "5777"
-
-const contractAddress = registryContractInfo.networks[NETWORK_ID].address
-
-const web3 = new Web3('HTTP://127.0.0.1:8545')
-
-let contractInstance = new web3.eth.Contract(contractInfo.abi, contractAddress)
-
-const pvtKey = Buffer.from('4909ceca58bff841f06a31671b84610faafe3ab5d674cc4c4715f81fea38a47b', 'hex')
-const account = web3.eth.accounts.privateKeyToAccount('0x' + pvtKey.toString('hex'));
-
-const TOKENS_TO_ADD = [
-  batContractInfo.networks[NETWORK_ID].address,
-  daiContractInfo.networks[NETWORK_ID].address,
-  golemContractInfo.networks[NETWORK_ID].address,
-  leoContractInfo.networks[NETWORK_ID].address,
-  omiseGoContractInfo.networks[NETWORK_ID].address,
-  tetherContractInfo.networks[NETWORK_ID].address,
-  wEthContractInfo.networks[NETWORK_ID].address,
-  zeroXContractInfo.networks[NETWORK_ID].address,
+const defaultProvider = 'HTTP://127.0.0.1:8545'
+const pvtKeyDefault = '4909ceca58bff841f06a31671b84610faafe3ab5d674cc4c4715f81fea38a47b'
+const contractAddressDefault = _get(registryContractInfo.networks[NETWORK_ID], 'address', '')
+const tokensDefault = [
+  _get(batContractInfo.networks[NETWORK_ID],'address',''),
+  _get(daiContractInfo.networks[NETWORK_ID],'address',''),
+  _get(golemContractInfo.networks[NETWORK_ID],'address',''),
+  _get(leoContractInfo.networks[NETWORK_ID],'address',''),
+  _get(omiseGoContractInfo.networks[NETWORK_ID],'address',''),
+  _get(tetherContractInfo.networks[NETWORK_ID],'address',''),
+  _get(wEthContractInfo.networks[NETWORK_ID],'address',''),
+  _get(zeroXContractInfo.networks[NETWORK_ID],'address',''),
 ]
+
+const options = commandLineArgs([
+  { name: 'provider', alias: 'p', type: String, defaultOption: defaultProvider },
+  { name: 'privatekey', alias: 'k', type: String, defaultOption: pvtKeyDefault },
+  { name: 'contract', alias: 'c', type: String, defaultOption: contractAddressDefault },
+  { name: 'token', alias: 't', type: String, multiple: true, defaultOption: tokensDefault },
+])
+
+const { provider, privatekey, contract, token } = options
+const web3 = new Web3(provider)
+
+let contractInstance = new web3.eth.Contract(contractInfo.abi, contract)
+
+const pvtKey = Buffer.from(privatekey, 'hex')
+const account = web3.eth.accounts.privateKeyToAccount('0x' + pvtKey.toString('hex'));
 
 function sendSignedTx(functionAbi) {
   return new Promise(async (resolve, reject) => {
@@ -77,7 +86,7 @@ async function addTokens(tokens) {
 }
 
 async function seed() {
-  await addTokens(TOKENS_TO_ADD)
+  await addTokens(token)
 }
 
 seed()
